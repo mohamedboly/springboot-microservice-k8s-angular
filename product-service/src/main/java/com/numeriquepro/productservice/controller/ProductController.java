2cc24f10d63256156a5952831bd7a3405a76ed26
Mohamadou Boly <mohamedbolydev@gmail.com>
2025-09-01 02:00:21 -0400

    add gitignore

---
+++ product-service/src/main/java/com/numeriquepro/productservice/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.numeriquepro.productservice.controller;
+
+
+import com.numeriquepro.productservice.dto.ProductRequest;
+import com.numeriquepro.productservice.dto.ProductResponse;
+import com.numeriquepro.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
---
+++ product-service/src/main/java/com/numeriquepro/productservice/dto/ProductRequest.java
@@ -0,0 +1,6 @@
+package com.numeriquepro.productservice.dto;
+
+import java.math.BigDecimal;
+
+public record ProductRequest(String id, String name, String description,
+                             String skuCode, BigDecimal price) { }
